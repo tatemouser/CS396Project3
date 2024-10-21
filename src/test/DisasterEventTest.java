f12e6317f4c5f51981f831970a93362bee568f8d
tatemouser <tatesmouser@gmail.com>
2024-10-20 21:22:41 -0500

    IGNORE

--- src/test/DisasterEventTest.java
+++ src/test/DisasterEventTest.java
@@ -1,21 +1,46 @@
 package test;
 
 import main.models.DisasterEvent;
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class DisasterEventTest {
+    private DisasterEvent event;
+
+    @Before
+    public void setUp() {
+        event = new DisasterEvent("Flood", "New Orleans");
+    }
+
     @Test
     public void testDisasterEventCreation() {
-        DisasterEvent event = new DisasterEvent("Flood", "New Orleans");
         assertEquals("Flood", event.getEventType());
         assertEquals("New Orleans", event.getLocation());
     }
 
     @Test
+    public void testSetEventType() {
+        event.setEventType("Hurricane");
+        assertEquals("Hurricane", event.getEventType());
+    }
+
+    @Test
+    public void testSetLocation() {
+        event.setLocation("Miami");
+        assertEquals("Miami", event.getLocation());
+    }
+
+    @Test
     public void testToString() {
-        DisasterEvent event = new DisasterEvent("Hurricane", "Miami");
-        String expected = "Disaster Event - Type: Hurricane, Location: Miami";
+        String expected = "DisasterEvent{eventType='Flood', location='New Orleans'}";
         assertEquals(expected, event.toString());
+    }
+
+    @Test
+    public void testNullValues() {
+        DisasterEvent nullEvent = new DisasterEvent(null, null);
+        assertNull(nullEvent.getEventType());
+        assertNull(nullEvent.getLocation());
     }
 }
